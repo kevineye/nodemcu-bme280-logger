2dfe18bb5c1c4cee02583be1d0d1db650e1f0004
Kevin Eye <eye@buffalo.edu>
2017-03-10 21:22:21 -0500

    upadted to use lib submodule

---
+++ app.lua
@@ -0,0 +1,18 @@
+PIN_LED         = 4
+
+local log = require 'log'
+log.level = 7
+
+require 'config'
+require 'ready'
+
+local app = {}
+
+function app.run()
+    require 'telnet'
+    require 'mqtt-connect'
+    require 'sensors'
+    ready = ready - 1
+end
+
+return app
